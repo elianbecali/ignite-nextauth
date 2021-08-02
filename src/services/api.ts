9cb4403d584dd94488f0a6fff2781183488951e4
Elian Becali <elianbecaliaguiar@gmail.com>
2021-08-02 09:47:08 -0400

    feat: add queue request after refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 
+type FailedRequest = {
+  onSuccess: (token: string) => void;
+  onFailure: (err: AxiosError) => void;
+};
+
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue: FailedRequest[] = [];
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -18,31 +25,61 @@ api.interceptors.response.use(
         cookies = parseCookies();
 
         const { 'nextauth.refreshToken': refreshToken } = cookies;
+        const originalConfig = error.config;
 
-        api
-          .post('refresh', {
-            refreshToken
-          })
-          .then((response) => {
-            const { token } = response.data;
+        if (!isRefreshing) {
+          isRefreshing = true;
 
-            setCookie(undefined, 'nextauth.token', token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: '/'
-            });
+          api
+            .post('refresh', {
+              refreshToken
+            })
+            .then((response) => {
+              const { token } = response.data;
 
-            setCookie(
-              undefined,
-              'nextauth.refreshToken',
-              response.data.refreshToken,
-              {
+              setCookie(undefined, 'nextauth.token', token, {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: '/'
-              }
-            );
+              });
 
-            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+              setCookie(
+                undefined,
+                'nextauth.refreshToken',
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: '/'
+                }
+              );
+
+              api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach((request) =>
+                request.onSuccess(token)
+              );
+              failedRequestsQueue = [];
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request) => request.onFailure(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers['Authorization'] = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            }
           });
+        });
       } else {
         // Deslogar o usuário
       }
