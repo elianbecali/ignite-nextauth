bd370893501d45d0297e92e4eb4b6c2cc4fb5e15
Elian Becali <elianbecaliaguiar@gmail.com>
2021-08-05 09:58:08 -0400

    feat: retrieve token from server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { parseCookies } from 'nookies';
 import { FormEvent, useContext, useState } from 'react';
+
 import { AuthContext } from '../contexts/AuthContext';
 
 export default function Home() {
@@ -40,3 +43,20 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+};
