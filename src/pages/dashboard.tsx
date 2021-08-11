90eee7cf03a3cce48ce3f42b43c83c3f070e918d
Elian Becali <elianbecaliaguiar@gmail.com>
2021-08-11 08:35:43 -0400

    feat: validate permissions by server

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,12 @@
 import { useContext } from 'react';
-import { Can } from '../components/Can';
 
 import { AuthContext } from '../contexts/AuthContext';
-import { useCan } from '../hooks/useCan';
+import { Can } from '../components/Can';
 import { setupAPIClient } from '../services/api';
 import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.list'],
-    roles: ['administrator', 'editor']
-  });
 
   return (
     <>
